e9660024ea0fdd533cf2024631526ebc93e3b5e0
Thomas Waksman <82084984+twaksman001@users.noreply.github.com>
2023-10-04 15:28:57 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -5,6 +5,5 @@ st.set_page_config(layout="wide")
 st.title(":house: Effectors Dashboard")
 st.write('Reference: https://www.biorxiv.org/content/10.1101/2023.10.02.560451v1')
 st.write('This app is for interacting with the effector bioinformatics data. '
-
 		 'For protein structure viewing, PyMOL is best.')
 st.write('Please navigate to the desired page (sidebar).')
