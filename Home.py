b2e8ec25e1e5fec2aa87151d7691564d79eeb778
Thomas Waksman <82084984+twaksman001@users.noreply.github.com>
2024-01-11 11:36:02 +0000

    misc

--- Home.py
+++ Home.py
@@ -5,5 +5,5 @@ st.set_page_config(layout="wide")
 st.title(':house: Green peach aphid salivary effectors Streamlit app')
 st.write('Reference: https://apsjournals.apsnet.org/doi/abs/10.1094/MPMI-10-23-0154-FI)')
 st.markdown('This app is for interacting with the *Myzus persicae* effector bioinformatics data. '
-		 'For protein structure viewing, PyMOL is best.')
+		 	'For protein structure viewing, PyMOL is best.')
 st.write('Please navigate to the desired page (sidebar).')
